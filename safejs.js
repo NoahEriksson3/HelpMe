757d9135873b59b3307689641322d47f0b0a9a7e
NoahEriksson3 <50990460+NoahEriksson3@users.noreply.github.com>
2020-02-19 17:36:09 +0100

    Testing Phase 1

--- safejs.js
+++ safejs.js
@@ -1,3 +1,7 @@
 function fuckyou() {
     
+        // Encoded String
+        var encodedStr = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xRl9aNnFudnU4ekFwaU44Nk9hclVuWmYyR3ljV25hekQvdmlldz91c3A9c2hhcmluZw==";
+        
+        // Decode the String
 }
